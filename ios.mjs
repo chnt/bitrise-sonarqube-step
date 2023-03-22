5f630dd24f9bbfc763ab30d3abe2a66b1b457ab7
Johnny Sørensen <joso@nodes.dk>
2023-03-22 14:17:04 +0100

    fix: make code coverage optional on ios

--- ios.mjs
+++ ios.mjs
@@ -35,19 +35,23 @@ export function writeConfiguration(config) {
 }
 
 export async function preScan() {
-    console.log('Fetching code coverage converter script...')
-    const fileName = 'xccov-to-sonarqube-generic.sh'
-    const convertedCoveragePath = 'cov.xml'
-    const response = await fetch('https://raw.githubusercontent.com/SonarSource/sonar-scanning-examples/master/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh')
-    const fileContent = await response.text()
-    fs.outputFileSync(fileName, fileContent)
-    await $`chmod 755 ${fileName}`;
-    console.log('Done')
+    if (process.env.BITRISE_XCRESULT_PATH) {
+        console.log('Fetching code coverage converter script...')
+        const fileName = 'xccov-to-sonarqube-generic.sh'
+        const convertedCoveragePath = 'cov.xml'
+        const response = await fetch('https://raw.githubusercontent.com/SonarSource/sonar-scanning-examples/master/swift-coverage/swift-coverage-example/xccov-to-sonarqube-generic.sh')
+        const fileContent = await response.text()
+        fs.outputFileSync(fileName, fileContent)
+        await $`chmod 755 ${fileName}`;
+        console.log('Done')
 
-    console.log('Installing jq via Homebrew...')
-    await $`brew install jq`
+        console.log('Installing jq via Homebrew...')
+        await $`brew install jq`
 
-    console.log('Converting XCRESULT to generic SonarQube format...')
-    await $`./${fileName} ${process.env.BITRISE_XCRESULT_PATH} > ${convertedCoveragePath}`
-    config.coverage_path = convertedCoveragePath
+        console.log('Converting XCRESULT to generic SonarQube format...')
+        await $`./${fileName} ${process.env.BITRISE_XCRESULT_PATH} > ${convertedCoveragePath}`
+        config.coverage_path = convertedCoveragePath
+    } else {
+        console.log('No XCRESULT_PATH found, skipping coverage convertion...')
+    }
 }
